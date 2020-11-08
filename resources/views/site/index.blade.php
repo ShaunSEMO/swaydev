04ca8c24783cd4d2674ac636029f1268950fa8ad
Tshego Moloto <tshegofatsosemenya@gmail.com>
2020-11-08 21:00:23 +0200

    Building skeleton

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,180 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Sway Dev | We Do Web And Software</title>
+  </head>
+  <body>
+    <div class="container-fluid header">
+        <nav data-aos="fade-down" class="navbar navbar-light bg-light navbar-expand-lg">
+            <a class="navbar-brand" href="{{ url('/') }}">
+              <img src="{{ asset('imgs/sd_logo.png') }}" width="100" class="d-inline-block align-top" alt="">
+            </a>
+  
+                <br><br>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+  
+            <div class="collapse navbar-collapse" id="navbarNav">
+  
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Get A Website</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+  
+            </div>
+        </nav>
+
+        <div class="row header-content">
+
+            <div class="col-md-6 header-headers">
+              <h2 class="heading">YOUR <span class="font-blue">ONLINE</span> PRESENCE MATTERS</h2>
+              <br>
+              <p class="subheading">Sway Dev puts your name on the web with elegant, professional and dynamic websites.</p>
+              <br>
+        
+              <div class="row container">
+                <div class="col-xs-6 button-div">
+                  <a href="{{ url('/getwebsite') }}" class="btn btn-primary bttn-primary">Order A Website</a>
+                </div>
+                <br>
+                <br>
+                <div class="col-xs-6 button-div button-contact ">
+                  <a href="tel:0623629473" class="btn btn-primary bttn-secondary"><i class="fas fa-phone"></i></a>
+                </div>
+                <br>
+                <div class="col-xs-6 button-div button-contact ">
+                  <a href="mailto:tshego@swaydev.co.za" class="btn btn-danger bttn-secondary"><i class="fas fa-envelope"></i></a>
+                </div>
+              </div>
+              <br>
+              <br>
+        
+            </div>
+        
+            
+        
+            <div id="carousel-example-multi" class="carousel slide carousel-multi-item v-2 col-sm-6 header-projects" data-ride="carousel">
+            
+            
+              <div class="carousel-inner v-2" role="listbox">
+                <h3>See What We Do</h3>
+                <hr class="section-hr">
+                <br>
+            
+                <div class="carousel-item active">
+                  <div class="col-12 col-md-4 example-project">
+                    <div class="container">
+                      <img class="img-fluid" src="/">
+                      <br><br>
+                      <h5>Kasie FM</h5>
+                      <a href="www.kasiefm.co.za">www.kasiefm.co.za</a>
+                    </div>
+                  </div>
+                </div>
+        
+                <div class="carousel-item">
+                    <div class="col-12 col-md-4 example-project">
+                        <div class="container">
+                          <img class="img-fluid" src="/">
+                          <br><br>
+                          <h5>Valrey Nkoana Personal Website</h5>
+                          <a href="www.valreynkoana.co.za">www.valreynkoana.co.za</a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="carousel-item">
+                    <div class="col-12 col-md-4 example-project">
+                        <div class="container">
+                          <img class="img-fluid" src="/">
+                          <br><br>
+                          <h5>Regatammogo</h5>
+                          <a href="www.regatammogo.co.za">www.regatammogo.co.za</a>
+                        </div>
+                    </div>
+                </div>
+        
+              </div>
+        
+              <br><br>
+              
+              <!--Controls-->
+              <div class="btn-group" role="group" aria-label="Basic example" style="margin:auto; width: 140px;">
+                <a style="margin:auto;" class="btn btn-primary custom-control" href="#carousel-example-multi" data-slide="prev"><i class="fas fa-chevron-left"></i></a>
+                <a style="margin:auto;" class="btn btn-primary custom-control" href="#carousel-example-multi" data-slide="next"><i class="fas fa-chevron-right"></i></a>
+              </div>
+              <!--/.Controls-->
+            
+            </div>
+        
+          </div>
+    </div>
+
+    <div class="container reasons-container">
+
+        <h3 class="section-header">Why a website is crucial for you.</h3>
+        <hr class="section-hr">
+        <br><br><br>
+        <div class="row">
+            <div class="col-md-4">
+                <span>ICON</span>
+                <br><br>
+                <h4>Professional</h4>
+                <br>
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin word</p>
+                <br><br>
+            </div>
+
+            <div class="col-md-4">
+                <span>ICON</span>
+                <br><br>
+                <h4>Accessible</h4>
+                <br>
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin word</p>
+                <br><br>
+            </div>
+
+            <div class="col-md-4">
+                <span>ICON</span>
+                <br><br>
+                <h4>Views and Impressions</h4>
+                <br>
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin word</p>
+                <br><br>
+            </div>
+        </div>
+      
+      </div>
+      
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
